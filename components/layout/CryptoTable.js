1d8cc781f2667ca3332df871a8d4da3973859317
nikhilnigamnik <nikhilnigamnik@gmail.com>
2024-04-22 23:50:48 +0530

    fix : pagination page limit count

--- components/layout/CryptoTable.js
+++ components/layout/CryptoTable.js
@@ -40,7 +40,7 @@ const CryptoTable = () => {
     setLoading(true);
     try {
       const res = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=${page}&sparkline=false&price_change_percentage=24h%2C7d`
       );
       const data = await res.json();
       setData(data);
